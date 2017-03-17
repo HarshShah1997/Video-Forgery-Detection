95c06a694d870e0e4e6ab91a0bbea94480481789
Harsh Shah <harsh.shah.jayesh@gmail.com>
2017-03-17 19:41:21 +0530

    Added extracting features

--- extract_features.m
+++ extract_features.m
@@ -1,1 +1,27 @@
-extract_features_file('Variables/01_original.mat');  
+cover_features = [];
+stego_features = [];
+
+feature = extract_features_file('Variables/01_original.mat');  
+cover_features = [cover_features feature];
+
+feature = extract_features_file('Variables/01_forged.mat');
+stego_features = [stego_features feature];
+
+feature = extract_features_file('Variables/02_original.mat');
+cover_features = [cover_features ; feature];
+
+feature = extract_features_file('Variables/02_forged.mat');
+stego_features = [stego_features ; feature];
+
+feature = extract_features_file('Variables/03_original.mat');
+cover_features = [cover_features ; feature];
+
+feature = extract_features_file('Variables/03_forged.mat');
+stego_features = [stego_features ; feature];
+
+
+
+
+
+
+
--- extract_features_file.m
+++ extract_features_file.m
@@ -1,10 +1,13 @@
-function [ans] = extract_features_file(filename)
+function [features] = extract_features_file(filename)
 struct_image = load(filename);
 field = fieldnames(struct_image);
-ans = getfield(struct_image,field{1});
+all_images = getfield(struct_image,field{1});
+features = [];
 
-for i=1:size(ans,1)
-    img = reshape(ans(i,:,:), [size(ans,2), size(ans,3)]);
-    subplot(4, 2, i), imshow(img);
+for i=1:size(all_images,1)
+    img = reshape(all_images(i,:,:), [size(all_images,2), size(all_images,3)]);
+    imwrite(img, 'temp.jpg');
+    features(i, :) = ccpev548('temp.jpg', 75);
+    delete 'temp.jpg';
 end
 end
