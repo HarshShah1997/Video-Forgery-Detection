68af0450252777a92bc12bef07d1374ee108a09a
Harsh Shah <harsh.shah.jayesh@gmail.com>
2017-03-17 17:05:39 +0530

    Finished creating residuals

---
+++ main_residuals.m
@@ -0,0 +1,21 @@
+
+%original_01 = create_residuals_video('Videos/01_original_enc10/', 210);
+%forged_01 = create_residuals_video('Videos/01_forged_enc10/', 210);
+
+%original_02 = create_residuals_video('Videos/02_original_enc10/', 329);
+%forged_02 = create_residuals_video('Videos/02_forged_enc10/', 329);
+
+%original_03 = create_residuals_video('Videos/03_original_enc10/', 313);
+%forged_03 = create_residuals_video('Videos/03_forged_enc10/', 313);
+
+%original_04 = create_residuals_video('Videos/04_original_enc10/', 319);
+%forged_04 = create_residuals_video('Videos/04_forged_enc10/', 319);
+
+%original_06 = create_residuals_video('Videos/06_original_enc10/', 262);
+%forged_06 = create_residuals_video('Videos/06_forged_enc10/', 262);
+
+%original_08 = create_residuals_video('Videos/08_original_enc10/', 274);
+%forged_08 = create_residuals_video('Videos/08_forged_enc10/', 274);
+
+%original_10 = create_residuals_video('Videos/08_original_enc10/', 239);
+%forged_10 = create_residuals_video('Videos/08_forged_enc10/', 239);
